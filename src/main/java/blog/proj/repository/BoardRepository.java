33110f56ada9cc8e7a6530a820f46083284d906c
hmkwon <gurals04@gmail.com>
2024-09-21 22:14:23 +0900

    repository interface commit

--- src/main/java/blog/proj/repository/BoardRepository.java
+++ src/main/java/blog/proj/repository/BoardRepository.java
@@ -1,4 +1,7 @@
 package blog.proj.repository;
 
-public class BoardRepository {
+import blog.proj.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
 }
