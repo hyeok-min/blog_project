7f5aa2fe90c112605db187f13e44f18da36cceba
hmkwon <gurals04@gmail.com>
2024-11-19 17:26:25 +0900

    board 전체 수정

--- src/main/java/blog/proj/repository/BoardCustomRepositoryImpl.java
+++ src/main/java/blog/proj/repository/BoardCustomRepositoryImpl.java
@@ -21,15 +21,16 @@ import java.util.List;
 public class BoardCustomRepositoryImpl implements BoardCustomRepository{
     private final JPAQueryFactory jpaQueryFactory;
 
+    //게시물 조회
     public BoardDto findByBoard(Long id){
-
+        //조회수 1 올리기
         long updateView = jpaQueryFactory
                 .update(board)
                 .set(board.view, board.view.add(1))  // 조회수 증가
                 .where(board.id.eq(id))
                 .execute();
 
-
+        //게시물 반환
         return jpaQueryFactory
                 .select(Projections.fields(BoardDto.class,board.id,board.name,board.title,board.content,folder.folderName,board.boardUpdateCount,board.view))
                 .from(board)
@@ -38,14 +39,40 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository{
                 .fetchOne();
     }
 
+    //특정 블로거의 메인 게시물 리스트(조회수 top 9)
+    public List<BoardDto> findByBoardListTop9(String name){
+        //게시물 반환
+        return jpaQueryFactory
+                .select(Projections.fields(BoardDto.class,board.id,board.name,board.title,board.content,board.view))
+                .from(board)
+                .where(board.name.eq(name))
+                .orderBy(board.view.desc()) // board.view를 내림차순으로 정렬
+                .limit(9) // 상위 9개만 가져오기
+                .fetch();
+    }
+
+    //블로거 전체의 메인 게시물 리스트(조회수 top 9)
+    public List<BoardDto> findByBoardListTop9All(){
+        //게시물 반환
+        return jpaQueryFactory
+                .select(Projections.fields(BoardDto.class,board.id,board.name,board.title,board.content,board.view))
+                .from(board)
+                .orderBy(board.view.desc()) // board.view를 내림차순으로 정렬
+                .limit(9) // 상위 9개만 가져오기
+                .fetch();
+    }
+
+    //게시물 리스트
     public List<BoardDto> findByBoardList(String name,Long id){
         return jpaQueryFactory
                 .select(Projections.fields(BoardDto.class,board.id,board.name,board.title,board.content,folder.folderName,board.view))
                 .from(board)
                 .join(board.folder,folder)
-                .where(board.name.eq(name).and(board.folder.id.eq(id)))
+                .where(board.folder.id.eq(id))
                 .fetch();
     }
+
+    //폴더 리스트
     public List<FolderDto> findByFolderList(String name, Long id){
         return jpaQueryFactory
                 .select(Projections.fields(FolderDto.class,folder.id,folder.folderName))
@@ -54,6 +81,7 @@ public class BoardCustomRepositoryImpl implements BoardCustomRepository{
                 .where(folder.category.eq(Category.valueOf("FOLDER")).and(user.id.eq(id)))
                 .fetch();
     }
+
     //검색
     public List<BoardDto> findBySearch(String query){
         return jpaQueryFactory
